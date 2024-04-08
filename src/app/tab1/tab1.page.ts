762dff3898cd64885cd36245b39486e1d719924e
Matheus Gomes <matheus.v.gomes00@gmail.com>
2024-04-08 16:32:27 -0300

    Mensagem do commit

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
+import { SenhasService } from '../services/senhas.service';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+  styleUrls: ['tab1.page.scss'],
+
 })
 export class Tab1Page {
-
-  constructor() {}
+  inputNovaSenha: string = '';
+  constructor(public senhasService: SenhasService){}
 
 }
+
--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { SenhasService } from '../services/senhas.service';
 
 @Component({
   selector: 'app-tab3',
@@ -6,7 +7,6 @@ import { Component } from '@angular/core';
   styleUrls: ['tab3.page.scss']
 })
 export class Tab3Page {
-
-  constructor() {}
+  constructor(public senhasService: SenhasService){}
 
 }
